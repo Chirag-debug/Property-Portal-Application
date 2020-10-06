6a1c8a44fc9057c763a6e109cdcb5291cc4b4c03
Chirag-debug <mulla.chirag@gmail.com>
2020-10-06 19:47:04 +0530

    buyer and seller classes updated

--- src/main/java/com/propertywalo/model/Buyer.java
+++ src/main/java/com/propertywalo/model/Buyer.java
@@ -1,4 +1,40 @@
 package com.propertywalo.model;
 
 public class Buyer {
+    private String firstName;
+    private String lastName;
+    private int mobileNo;
+    private int budget;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getMobileNo() {
+        return mobileNo;
+    }
+
+    public void setMobileNo(int mobileNo) {
+        this.mobileNo = mobileNo;
+    }
+
+    public int getBudget() {
+        return budget;
+    }
+
+    public void setBudget(int budget) {
+        this.budget = budget;
+    }
 }
--- src/main/java/com/propertywalo/model/Seller.java
+++ src/main/java/com/propertywalo/model/Seller.java
@@ -1,4 +1,49 @@
 package com.propertywalo.model;
 
 public class Seller {
+    private String firstName;
+    private String lastName;
+    private int mobileNo;
+    private int pinCode;
+    private int listedPrice;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getMobileNo() {
+        return mobileNo;
+    }
+
+    public void setMobileNo(int mobileNo) {
+        this.mobileNo = mobileNo;
+    }
+
+    public int getPinCode() {
+        return pinCode;
+    }
+
+    public void setPinCode(int pinCode) {
+        this.pinCode = pinCode;
+    }
+
+    public int getListedPrice() {
+        return listedPrice;
+    }
+
+    public void setListedPrice(int listedPrice) {
+        this.listedPrice = listedPrice;
+    }
 }
