9392fb094edfb99e145853d030e063fa4a1acd76
Chirag-debug <mulla.chirag@gmail.com>
2020-10-07 00:07:06 +0530

    updated Property model class

--- src/main/java/com/propertywalo/model/Property.java
+++ src/main/java/com/propertywalo/model/Property.java
@@ -1,4 +1,11 @@
 package com.propertywalo.model;
 
 public class Property {
+    private String city;
+    private int pinCode;
+    private String propertyTitle;
+    private String propertyType; // independent house, apartment, commercial shop, commercial office, plot, agriculture land
+    private double propertyRatePSF; // per sq ft rate
+    private double carpetArea;
+    private String configuragion; // 1RK, 1BHK, 2BHK, 3BHK, 4BHK
 }
